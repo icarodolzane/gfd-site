dfdfdbe43c03ef325e05c488c60c1e7ec3a4ae5d
icarodolzane <icarodolzane@live.com>
2023-01-29 19:24:44 -0400

    Refatorado com responsividade para mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,34 @@
 import logo from "../images/logo.png";
-
+import menu from "../icons/menu.png";
 import '../style/Header.css';
+
+const headerResponsive = (
+
+  <nav className="my-nav-responsive">
+    <div className="dropdown">
+      <button class="dropbtn">
+          <img src={menu} alt="menu-icon" />
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="/"> Início </a>
+            <a href="/about">Sobre a Empresa</a>
+            <a href="/services">Serviços</a>
+            <a href="/projects">Projetos</a>
+            <a href="/team">Equipe</a>
+            <a href="/contact">Contato</a>
+        </div>
+    </div>
+  </nav>
+);
+
 function Header() {
   return (
     <header className="my-header">
-      <div className="logo">
+      <a href="/" className="logo">
         <img src={ logo } alt="logo-gfd" />
-      </div>
+      </a>
+      {headerResponsive}
       <nav className="my-nav">
         <a href="/"> Início </a>
         <a href="/services"> Serviços </a>
